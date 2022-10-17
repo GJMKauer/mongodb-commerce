61c2f6932a740efc9dff20c71371d72b7546c778
Gabriel Kauer <gjmkauer@hotmail.com>
2022-10-17 16:28:02 -0300

    FEAT: add req22 - create query with updateMany, updateOne, set, inc and find in array.

---
+++ challenges/desafio22.js
@@ -0,0 +1,19 @@
+db.produtos.updateMany(
+  {},
+  { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } },
+);
+
+db.produtos.updateOne(
+  { nome: { $eq: "Big Mac" } },
+  { $inc: { "vendasPorDia.3": 60 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["bovino"] } },
+  { $inc: { "vendasPorDia.6": 120 } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, vendasPorDia: 1, _id: 0 },
+);
